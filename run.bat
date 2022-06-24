1df8a5763fcba5d9929fc4c883f3844b1de2f445
gelarpambudi <gelargelur@gmail.com>
2022-06-24 19:29:21 +0700

    fix startup script

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ echo "Installing depedencies..."
 pip install -r requirements.txt
 
 echo "Initialize App Config..."
-set CONFIG_PATH=C:Users\ADMIN\Desktop\config.yaml
+set CONFIG_PATH=C:\Users\ADMIN\Desktop\config.yaml
 
 echo "Starting Python Photobooth Service..."
 python main.py
